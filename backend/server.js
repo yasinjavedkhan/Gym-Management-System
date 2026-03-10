5040305bca722d27be8862de836e407aa6e24b63
yasinjavedkhan <yasinjavedkhan@users.noreply.github.com>
2026-03-10 16:20:22 +0530

    Update default port to 10000 for Render compatibility

--- backend/server.js
+++ backend/server.js
@@ -38,7 +38,7 @@ app.use((err, req, res, next) => {
 //  console.log(`Server running on port ${PORT}`);
 //});
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 
 app.listen(PORT, () => {
     console.log("Server running on port " + PORT);
